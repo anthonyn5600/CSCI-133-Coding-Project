a340e1923e18d3519e7d83e54c3e54977a7ccb49
Albert Lee <albertlee0622@gmail.com>
2019-02-26 15:47:12 -0800

    testing to make a little change

--- Virtual Piano Project/Virtual Piano Project/piano.cpp
+++ Virtual Piano Project/Virtual Piano Project/piano.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-	cout << "testing if it works correctly.";
+	cout << "testing if it works correctly.\n";
+	cout << "testing to make a little change";
 	cout << endl;
 	system("pause");
 	return 0;
