3077174d5e5c98ac9103cac8fdcf7d96567063d2
Albert Lee <albertlee0622@gmail.com>
2019-02-26 15:42:28 -0800

    Initial Commit

--- Virtual Piano Project/Virtual Piano Project/piano.cpp
+++ Virtual Piano Project/Virtual Piano Project/piano.cpp
@@ -1,1 +1,11 @@
-//testing
+//testing Albert Lee
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "testing if it works correctly.";
+	cout << endl;
+	system("pause");
+	return 0;
+}
