303722513caa6f4179a2725be25945c6825598c2
anthonyn5600 <37987501+anthonyn5600@users.noreply.github.com>
2019-03-01 14:15:57 -0800

    Alright, just testing if you can see this.

--- Virtual Piano Project/Virtual Piano Project/piano.cpp
+++ Virtual Piano Project/Virtual Piano Project/piano.cpp
@@ -7,6 +7,11 @@ int main()
 	cout << "testing if it works correctly.\n";
 	cout << "testing to make a little change";
 	cout << endl;
+	
+
+	//anthony
+	cout << "can you see this text in the github? \n";
+
 	system("pause");
 	return 0;
 }
