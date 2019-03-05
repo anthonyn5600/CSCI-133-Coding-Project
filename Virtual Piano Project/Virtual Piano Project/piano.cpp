5b612e533e6b28d729fb2f1c5678198bcf69975f
anthonyn5600 <37987501+anthonyn5600@users.noreply.github.com>
2019-03-04 16:36:15 -0800

    New SFML codes.

--- Virtual Piano Project/Virtual Piano Project/piano.cpp
+++ Virtual Piano Project/Virtual Piano Project/piano.cpp
@@ -1,17 +1,93 @@
-//testing Albert Lee
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
-	cout << "testing if it works correctly.\n";
-	cout << "testing to make a little change";
-	cout << endl;
-	
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
+	sf::Text text;
+	sf::Font font;
+	sf::SoundBuffer buffer;
+	sf::Sound sound;
+	sf::Texture texture;
+	sf::Sprite sprite;
 
-	//anthony
-	cout << "can you see this text in the github? \n";
+	int timer = 0;
 
-	system("pause");
+	string changer = "sprites/keyboard.gif";
+
+
+	sprite.setTexture(texture);
+	sprite.setTextureRect(sf::IntRect(10, 10, 1000, 400));
+	sprite.setPosition(sf::Vector2f(730.f, 450.f));
+
+	sound.setBuffer(buffer);
+
+
+
+	if (!buffer.loadFromFile("Yamaha-V50-IceBell-C5.wav"))
+		cout << "Error in loading sound \n";
+
+	font.loadFromFile("fonts/OpenSans-Regular.ttf");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+
+			if (!texture.loadFromFile(changer))
+				cout << "Error in loading textures \n";
+
+			// select the font
+			text.setFont(font); // fontg is a sf::Font
+
+			// set the string to display
+			text.setString("Virtual Piano");
+
+			// set the character size
+			text.setCharacterSize(25); // in pixels, not points!
+
+			// set the color
+			text.setFillColor(sf::Color::Red);
+
+			// set the text style
+			text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			{
+				changer = "sprites/keyboarda.gif";
+				sound.play();
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+			{
+				changer = "sprites/keyboards.gif";
+				sound.play();
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			{
+				changer = "sprites/keyboardd.gif";
+				sound.play();
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+			{
+				changer = "sprites/keyboarde.gif";
+				sound.play();
+			}
+		}
+
+		window.clear();
+		window.draw(sprite);
+		window.draw(text);
+		window.display();
+	}
+
 	return 0;
 }
